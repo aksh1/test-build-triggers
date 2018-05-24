12290114431a9d5bd2d25103a57bddbdabcc646e
timus <sumit.s@shieldsquare.com>
2018-05-24 13:22:30 +0530

    cloudbuild.yaml , env var added

--- say-hello.py
+++ say-hello.py
@@ -1,10 +1,12 @@
 import time
 import BaseHTTPServer
+from os import environ
 
 
 HOST_NAME = '0.0.0.0' # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 8080 # Maybe set this to 9000.
-
+e1=environ.get('ENV1')
+e2=environ.get('ENV2')
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
@@ -19,10 +21,14 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.wfile.write("<html><head><title>Build-trigger-test</title></head>")
         s.wfile.write("<h2> When I say Hey </h1><br>")
         s.wfile.write("<h2> You say </h2>")
-        s.wfile.write("<h3> Baebbyyyy </h3>")
+        s.wfile.write("<h3> "+e1+" </h3>")
+        s.wfile.write("<h3> "+e2+" </h3>")
+
         s.wfile.write("</body></html>")
 
 if __name__ == '__main__':
+	
+
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
     print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
